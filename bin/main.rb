d6bfcd7081022f3c6395b61a32787e568f936eb4
uwadonat <uwadonat@gmail.com>
2021-02-23 10:51:45 +0200

    FIX RUBOCOP ERRORS

--- bin/main.rb
+++ bin/main.rb
@@ -1,11 +1,12 @@
 #!/usr/bin/env ruby
 require_relative '../lib/settings'
 require_relative '../lib/logic'
-include Player
+
 def players(sign)
+  player = Player.new
   state = true
   while state
-    if Player.sign_validate(sign)
+    if player.sign_validate(sign)
       puts "player #{sign} make the move"
       break
     else
@@ -17,9 +18,10 @@ def players(sign)
 end
 
 def move_check(move, array, var)
+  player = Player.new
   state = true
   while state
-    if Player.move_validate(move) && Player.index_taken(move - 1, array)
+    if player.move_validate(move) && player.index_taken(move - 1, array)
       array[move - 1] = var
       break
     else
@@ -33,20 +35,21 @@ array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 puts 'choose the player (1 is x) or (2 is o)'
 sign = gets.chomp
 sign = players(sign)
+player = Player.new
 i = 0
 while i < 9
   move = gets.chomp.to_i
   move_check(move, array, sign)
   puts b.draw_board(array).to_s
-  if Player.wins(array, sign)
+  if player.wins(array, sign)
     puts "player #{sign} wins \u{1F911}"
     break
   end
   if i == 8
-    Player.draw
+    player.draw
     break
   end
-  sign = Player.flip_user(sign)
+  sign = player.flip_user(sign)
   puts "player #{sign} make the move"
   i += 1
 end
